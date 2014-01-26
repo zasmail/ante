e5f947f5835887f479055a52a035e2df57c614f4
Zo Asmail <zasmail@utexas.edu>
2014-01-25 23:52:37 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ante::Application.config.secret_key_base = '64d3f6a122b31a90a3efa71d1b03a246880575071411c309387769f4c14c22c43c7b8f203a139477c1dfc4470253fb02f2d4b9546b5111af554632a851313f87'
